1fb487a33ac3e0af3d91e03418dc57c800d2f66f
Khaled-AL-ALi <khaled2020pro@gmail.com>
2022-03-21 11:14:43 +0200

    finish fonctionality of admins table

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import Login from "./pages/Login"
-import { Routes, Route } from 'react-router-dom';
-import ProfitGoal from "./pages/ProfitGoal";
-import Reports from "./pages/Reports";
-import Transaction from "./pages/Transactions";
-import Nav from "./components/Nav";
+// import Login from "./pages/Login"
+// import { Routes, Route } from 'react-router-dom';
+// import ProfitGoal from "./pages/ProfitGoal";
+// import Reports from "./pages/Reports";
+// import Transaction from "./pages/Transactions";
+// import Nav from "./components/Nav";
+import AdminDachboard from "./components/AdminDachboard";
 function App() {
   return (
     <Routes>
@@ -13,8 +14,8 @@ function App() {
         <Route path="/home/transaction" element={<Transaction/>} />
         <Route path="/home/profitgoal" element={<ProfitGoal/>} /> 
           </Route>
-       
     </Routes>
+  
   );
 }
 
